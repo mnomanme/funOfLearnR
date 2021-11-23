6472dab183fb920bf81d089d9ca52037416718bf
nomanme <abdullah.noman083@gmail.com>
2021-11-24 00:07:05 +0600

    typesOfVariables

---
+++ CoreProgrammingPrinciples/variables.R
@@ -0,0 +1,23 @@
+
+
+#integer
+x <- 2L
+typeof(x)
+
+#double 
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 3 + 2i
+typeof(z)
+
+#character
+a <- "m"
+typeof(a)
+
+#logical
+q1 <- TRUE
+typeof(q1)
+q2 <- F 
+typeof(q2)
